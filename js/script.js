98b746903e434263895430377dbf8ee4b3226ce2
stekatag <stefangogov@protonmail.com>
2023-05-18 10:19:01 +0300

    Init commit

---
+++ js/script.js
@@ -0,0 +1,100 @@
+"use strict";
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const logoEl = document.querySelector(".logo");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const [ent] = entries;
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+      // logoEl.src = "img/justGo-logo-dark.png";
+    } else {
+      document.body.classList.remove("sticky");
+      // logoEl.src = "img/justGo-logo-light.png";
+    }
+  },
+  {
+    // Inside the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-60px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Making the mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Button scrolling
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav__link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+const swiper = new Swiper(".trending-cards-content", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  fade: true,
+  autoplay: {
+    delay: 2500,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".trending-button-next",
+    prevEl: ".trending-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    570: {
+      slidesPerView: 2,
+    },
+    780: {
+      slidesPerView: 3,
+    },
+    1250: {
+      slidesPerView: 4,
+    },
+  },
+});
